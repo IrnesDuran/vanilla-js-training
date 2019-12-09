3af88801df3a91991c18f592f807f5c0b819fbc3
darkhitect1985 <55331764+darkhitect1985@users.noreply.github.com>
2019-12-09 23:00:27 +0100

    Custom HTTP Library through Fetch with Async and Await

---
+++ easyhttp3/app.js
@@ -0,0 +1,30 @@
+const http =new EasyHTTP();
+
+//GET Users
+//  http.get('https://jsonplaceholder.typicode.com/users')
+//  .then(data => console.log(data))
+//  .catch(err => console.log(err))
+
+
+//UserData
+
+const data = {
+    name: 'John Doe',
+    username: 'johndoe',
+    email: 'johndoe@gmail.com'
+}
+
+//create post request
+// http.post('https://jsonplaceholder.typicode.com/users',data)
+//  .then(data => console.log(data))
+//  .catch(err => console.log(err));
+
+//create update to post
+//  http.put('https://jsonplaceholder.typicode.com/users/2', data)
+//  .then(data => console.log(data))
+//  .catch(err => console.log(err));
+
+//delete user
+ http.delete('https://jsonplaceholder.typicode.com/users/2')
+ .then(data => console.log(data))
+ .catch(err => console.log(err));
